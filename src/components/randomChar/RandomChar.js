53624a1f0850436400bc25282547d541d08d7872
Varvara <varya.kuch@gmail.com>
2022-06-06 23:00:50 +0500

    поправила отступ в коде

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -29,6 +29,7 @@ class RandomChar extends Component {
     onCharacterLoaded = (character) => {
         this.setState({character, loading: false});
     }
+    
     updateCharacter = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
