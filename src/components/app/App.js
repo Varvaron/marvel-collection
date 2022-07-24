43ee85e7abe5851c64f51967ee9bb715a1fc7f69
Varvara <varya.kuch@gmail.com>
2022-07-24 10:55:41 +0500

    разделение компонентов на страницы, создание компонентов для страниц, корректировка импортов в app.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,35 +1,15 @@
-import { useState } from "react";
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppHeader from '../appHeader/AppHeader';
+import MainPage from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage';
 
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedCharacter, setCharacter] = useState(null);
-
-    const onSelectedCharacter = (id) => {
-        setCharacter(id)
-    }
-
     return (
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onSelectedCharacter={onSelectedCharacter}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo characterId={selectedCharacter}/>
-                    </ErrorBoundary>  
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <MainPage/>
+                <ComicsPage/>
             </main>
         </div>
     )
