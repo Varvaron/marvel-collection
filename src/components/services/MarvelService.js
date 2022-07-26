34c1f73bfe500f2c4446c70aa0340ec54dc0c33f
Varvara <varya.kuch@gmail.com>
2022-07-26 21:07:02 +0500

    скорректированы мелкие ошибки

--- src/components/services/MarvelService.js
+++ src/components/services/MarvelService.js
@@ -16,12 +16,12 @@ class MarvelService {
 
     getAllCharacters = async (offset = this._baseOffset) => {
         const items = await this.getResource(`${this._apiBase}/characters?limit=9&offset=${offset}&${this._apiKey }`);
-        return items.data.results.map(this.changeCharacter);
+        return items.data.results.map(this._changeCharacter);
     }
 
     getCharacter = async (id) => {
         const item = await this.getResource(`${this._apiBase}/characters/${id}?&${this._apiKey}`);
-        return this.changeCharacter(item.data.results[0]);
+        return this._changeCharacter(item.data.results[0]);
     }
 
     getComics = async (id) => {
@@ -29,30 +29,29 @@ class MarvelService {
         return this._transformComics(res.data.results[0]);
     }
 
-    changeCharacter = (character) => {
-
-    return {
-        id: character.id,
-        name: character.name,
-        description: character.description ? character.description.slice(0, 225) : 'There is no description for this characher',
-        thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
-        homepage: character.urls[0].url,
-        wiki: character.urls[1].url,
-        comics: character.comics.items,
+    _changeCharacter = (character) => {
+        return {
+            id: character.id,
+            name: character.name,
+            description: character.description ? character.description.slice(0, 225) : 'There is no description for this characher',
+            thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
+            homepage: character.urls[0].url,
+            wiki: character.urls[1].url,
+            comics: character.comics.items,
+        }
     }
-  }
 
-  _transformComics = (comics) => {
-    return {
-        id: comics.id,
-        title: comics.title,
-        description: comics.description || 'There is no description',
-        pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
-        thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
-        language: comics.textObjects.language || 'en-us',
-        price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+    _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects.language || 'en-us',
+            price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+        }
     }
-}
 }
 
 export default MarvelService;
